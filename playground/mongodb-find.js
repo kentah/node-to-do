d346c4f733752d84265d4be672b980352be13442
kentah <kenthow@gmail.com>
2016-12-26 13:31:35 -0600

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,26 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({_id: new ObjectID('5855b963057ca411877df8f8')}).toArray()
+  //   .then((docs) => {
+  //     console.log('Todos');
+  //     console.log(JSON.stringify(docs, undefined, 2));
+  //   }, (err) => {
+  //     console.log('Unable to fetch todos', err);
+  //   });
+  //db.close();
+  db.collection('Users')
+    .find({name: 'Elias'})
+    .toArray()
+    .then((docs) => {
+      console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+      console.log('Unable to fetch todos', err);
+    });
+});

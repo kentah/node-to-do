188b023f0d46203feabe6decc75b8fc1c37f1662
kentah <kenthow@gmail.com>
2017-02-14 20:58:17 -0600

    Hash user pasword before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,15 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-
-var data = {
-  id: 10
-};
-
-var token = jwt.sign(data, '123abc');
-console.log(token)
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded)
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token)
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded)
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
@@ -35,3 +35,17 @@ console.log('decoded', decoded)
 // } else {
 //   console.log('Data was changed. Do not trust!')
 // }
+
+var password = '123abc!';
+//
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
+
+var hashedPasword = '$2a$10$/aIHP5szN/UvhX8s1wO4x.WevjkuMCej.ZT/fmab2RV6u969neDJK';
+
+bcrypt.compare(password, hashedPasword, (err, res) => {
+  console.log(res);
+});
